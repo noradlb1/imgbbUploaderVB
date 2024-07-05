d90b83fe0442927e320aeb3771442f9b3bd1f3c3
MONSTERMC <74623428+noradlb1@users.noreply.github.com>
2024-07-05 02:47:23 +0200

    imgbbUploaderVB
    imgbbUploaderVB

---
+++ Upload Image/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 257)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(272, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "d7d06f5722bd633dd45b68a4190a8602"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(65, 410)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(170, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 287)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(272, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(272, 228)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 358)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(272, 23)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.AutoSize = True
+        Me.StatusLabel.Location = New System.Drawing.Point(12, 383)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(37, 13)
+        Me.StatusLabel.TabIndex = 6
+        Me.StatusLabel.Text = "Status"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(11, 334)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(272, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(138, 387)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(145, 17)
+        Me.CheckBox1.TabIndex = 8
+        Me.CheckBox1.Text = "Save Image After Upload"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(8, 436)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(88, 13)
+        Me.LinkLabel1.TabIndex = 9
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "@MONSTERMC"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(295, 458)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Upload Image To imgbb"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents StatusLabel As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ Upload Image/Form1.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Net.Http
+Imports System.IO
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    'الأيميل sokniyirta@gufum.com
+    ' دعم السحب والإفلات
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Async Sub Form1_DragDrop(sender As Object, e As DragEventArgs) Handles MyBase.DragDrop
+        Try
+            Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+            If files.Length > 0 Then
+                Await UploadImageAsync(files(0))
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim openFileDialog As New OpenFileDialog()
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif"
+            openFileDialog.Multiselect = True ' للسماح باختيار ملفات متعددة
+
+            If openFileDialog.ShowDialog() = DialogResult.OK Then
+                For Each file In openFileDialog.FileNames
+                    Await UploadImageAsync(file)
+                Next
+            End If
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Async Function UploadImageAsync(imagePath As String) As Task
+        Dim apiKey As String = TextBox1.Text ' أدخل مفتاح API هنا أو في TextBox
+        If String.IsNullOrEmpty(apiKey) Then
+            MessageBox.Show("Please enter your API key.")
+            Return
+        End If
+
+        ProgressBar1.Style = ProgressBarStyle.Marquee
+        StatusLabel.Text = "Uploading image..."
+
+        Try
+            Dim client As New HttpClient()
+            Dim requestContent As New MultipartFormDataContent()
+            requestContent.Add(New StringContent(apiKey), "key")
+
+            If Not String.IsNullOrEmpty(imagePath) Then
+                Dim imageBytes As Byte() = File.ReadAllBytes(imagePath)
+                Dim base64Image As String = Convert.ToBase64String(imageBytes)
+                requestContent.Add(New StringContent(base64Image), "image")
+            End If
+
+            Dim response As HttpResponseMessage = Await client.PostAsync("https://api.imgbb.com/1/upload", requestContent)
+            response.EnsureSuccessStatusCode() ' This will throw an exception if the status code is not 2xx
+
+            Dim responseContent As String = Await response.Content.ReadAsStringAsync()
+
+            ' استخدام JObject لتحليل الاستجابة واستخراج رابط الصورة والتفاصيل الإضافية:
+            Dim jsonResponse As JObject = JObject.Parse(responseContent)
+            Dim imageUrl As String = jsonResponse("data")("url").ToString()
+            TextBox2.Text = imageUrl ' أو Label2.Text لعرض الرابط
+
+            ' عرض تفاصيل إضافية عن الصورة
+            Dim imageDetails As String = $"Size: {jsonResponse("data")("size")} bytes{Environment.NewLine}" &
+                                         $"Width: {jsonResponse("data")("width")}{Environment.NewLine}" &
+                                         $"Height: {jsonResponse("data")("height")}"
+            TextBox3.Text = imageDetails
+
+            ' تحميل الصورة وعرضها في PictureBox
+            Dim imageStream As Stream = Await client.GetStreamAsync(imageUrl)
+            PictureBox1.Image = Image.FromStream(imageStream)
+
+
+            If CheckBox1.Checked = True Then
+                ' حفظ الصورة محلياً بعد التحميل
+                Dim saveDialog As New SaveFileDialog()
+                saveDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif"
+                If saveDialog.ShowDialog() = DialogResult.OK Then
+                    PictureBox1.Image.Save(saveDialog.FileName)
+                End If
+            End If
+
+
+            StatusLabel.Text = "Image uploaded successfully."
+        Catch ex As Exception
+            MessageBox.Show("An error occurred: " & ex.Message)
+            StatusLabel.Text = "Error uploading image."
+        Finally
+            ProgressBar1.Style = ProgressBarStyle.Blocks
+        End Try
+    End Function
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://t.me/MONSTERMCSY")
+    End Sub
+End Class
